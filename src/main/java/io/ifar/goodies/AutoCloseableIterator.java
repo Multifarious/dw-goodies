40c2916a6e69be380484e628f76448b96ecf4f96
Lorrin Nelson <lhn@mult.ifario.us>
2013-05-20 13:08:36 -0700

    Add concat support to AutoCloseableIterator

--- src/main/java/io/ifar/goodies/AutoCloseableIterator.java
+++ src/main/java/io/ifar/goodies/AutoCloseableIterator.java
@@ -1,5 +1,8 @@
 package io.ifar.goodies;
 
+import com.google.common.collect.ImmutableList;
+
+import java.util.Collection;
 import java.util.Iterator;
 
 /**
@@ -11,6 +14,56 @@ import java.util.Iterator;
 public class AutoCloseableIterator<T> implements IterableIterator<T>, AutoCloseable {
     private final Iterator<T> wrapped;
     private final AutoCloseable closeHook;
+
+    /**
+     * Returns an AutoCloseableIterator that iterates over all the provided input iterators.
+     *
+     * If an Exception is thrown while closing an iterator, the other iterators are closed and the first Exception (only)
+     * is rethrown.
+     *
+     * @see com.google.common.collect.Iterators#concat(java.util.Iterator[])
+     * @param inputs
+     * @param <T>
+     * @return
+     */
+    public static <T> AutoCloseableIterator<T> concat(final AutoCloseableIterator<T>... inputs) {
+        return concat(ImmutableList.copyOf(inputs));
+    }
+
+    /**
+     * Returns an AutoCloseableIterator that iterates over all the provided input iterators.
+     *
+     * If an Exception is thrown while closing an iterator, the other iterators are closed and the first Exception (only)
+     * is rethrown.
+     *
+     * @see com.google.common.collect.Iterators#concat(java.util.Iterator[])
+     * @param inputs
+     * @param <T>
+     * @return
+     */
+    public static <T> AutoCloseableIterator<T> concat(final Collection<AutoCloseableIterator<T>> inputs) {
+        return new AutoCloseableIterator<T>(
+                com.google.common.collect.Iterators.concat(inputs.iterator()),
+                new AutoCloseable() {
+                    @Override
+                    public void close() throws Exception {
+                        Exception firstException = null;
+                        for (AutoCloseableIterator<? extends T> i : inputs) {
+                            try {
+                                i.close();
+                            } catch (Exception e) {
+                                if (firstException == null) {
+                                    firstException = e;
+                                }
+                            }
+                        }
+                        if (firstException != null) {
+                            throw firstException;
+                        }
+                    }
+                }
+        );
+    }
 
     /**
      * Wraps a standard Iterator with no-op close behavior.
